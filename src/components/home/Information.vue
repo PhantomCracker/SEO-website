34b98ae1bd921ae1df045dcd0ac032422961b4f3
Popa Vlad <popa.vlad94@gmail.com>
2021-08-25 19:38:55 +0300

    Adding the Information section

---
+++ src/components/home/Information.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="information">
+    <div class="container pt-5 pb-5">
+      <div class="row">
+        <h1 class="fw-bold">24/7 Emergency locksmith services</h1>
+        <h4 class="pt-3 pb-3 fw-lighter">We run the fastest response Locksmith Service all across the country – 24/7, all year round</h4>
+        <h6> When you call our Emergency number we will:
+          <ul class="pt-3 pb-3">
+            <li>✔ Answer immediately</li>
+            <li>✔ Be with you in less than 25 minutes</li>
+            <li>✔ Fix your emergency</li>
+          </ul>
+        </h6>
+        <p class="small-text">Looking for a cheap and near, 24 hour, same day, local emergency locksmith?</p>
+        <p>We provide the best quality locksmith services with multiple teams located near you, our services include business locksmith services, emergency locksmith services, 24-hour locksmith services, with us you’ll receive trusted locksmith services in the most urgent of manner.</p>
+        <p>Our Locksmiths teams are highly trained to provide security advice on a wide variety of applications. We supply and fit all major brands of lock and security fittings. Our 24/7 emergency call-out service will ensure your property is secured quickly and efficiently. We’re always ready to help our customers with the best team of professional locksmiths wherever you are, you can be assured we have the right team near you, ready to assist 24 hours, 7 days a week.</p>
+        <p class="small-text">Our locksmiths are located near you and only 25 minutes away. Labour from £49 and Locks from £25 with 12-months warranty.</p>
+        <button class="btn btn-primary my-primary-btn mt-4">
+          <a class="text-white text-decoration-none" href="tel:03333446467"><font-awesome-icon icon="phone-alt" style="margin-right: 10px" />Call now</a>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Information",
+  components: {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+  .information{
+    .container {
+      background-image: url("../../../public/images/bglayer_1.png");
+      background-size: cover;
+      background-repeat: no-repeat;
+
+    }
+    h3 {
+      font-weight: 200;
+    }
+    ul {
+      li {
+        list-style-type: none;
+        font-style: italic;
+      }
+    }
+    .small-text {
+      font-size: 1.5rem;
+    }
+    .my-primary-btn {
+      width: 25%;
+      margin: 0 auto;
+      @media screen and (max-width: 500px)  {
+        width: 70%;
+      }
+    }
+  }
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="home">
     <Hero />
-    <ServicesCard/>
+    <ServicesCard />
+    <Information />
   </div>
 </template>
 
 <script>
 import Hero from '../components/home/Hero';
 import ServicesCard from "../components/home/ServicesCard";
+import Information from "../components/home/Information";
 
 export default {
   name: "Home",
   components: {
     Hero,
-    ServicesCard
+    ServicesCard,
+    Information
   },
   methods: {}
 }
 </script>
 
 <style scoped>
+.span4 img {
+  margin-right: 10px;
+}
+.span4 .img-left {
+  float: left;
+}
+.span4 .img-right {
+  float: right;
+}
 </style>

1c29d91e50de5fb53b9764bbd5d9976be6ddf00b
Popa Vlad <popa.vlad94@gmail.com>
2021-08-26 08:46:36 +0300

    Adding carousel and Partners section

--- src/components/home/Partners.vue
+++ src/components/home/Partners.vue
@@ -1,34 +1,70 @@
 <template>
   <div class="partners">
-    <vue-carousel :data="data"></vue-carousel>
+    <vueper-slides
+      class="no-shadow"
+      autoplay
+      :arrows="false"
+      :visible-slides="3"
+      fixed-height="150px"
+      slide-multiple
+      :gap="3"
+      :slide-ratio="1 / 4"
+      :dragging-distance="200"
+      :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }"
+      style="background-size: auto; background-repeat: no-repeat">
+    <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.image" />
+    </vueper-slides>
   </div>
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
 export default {
   name: "Partners",
-  data() {
-    return {
-      data: [
-        '<div class="example-slide">Slide 1</div>',
-        '<div class="example-slide">Slide 2</div>',
-        '<div class="example-slide">Slide 3</div>',
-      ]
-    }
+  data: () => ({
+    slides: [
+      {
+        image: require('../../../public/images/partners/ABS.png')
+      },
+      {
+        image: require('../../../public/images/partners/ASSA-ABLOY.png')
+      },
+      {
+        image: require('../../../public/images/partners/Chub.png')
+      },
+      {
+        image: require('../../../public/images/partners/CISA.png')
+      },
+      {
+        image: require('../../../public/images/partners/ERA.png')
+      },
+      {
+        image: require('../../../public/images/partners/Ultion.png')
+      },
+      {
+        image: require('../../../public/images/partners/Union.png')
+      },
+      {
+        image: require('../../../public/images/partners/YALE.png')
+      },
+      {
+        image: require('../../../public/images/partners/ABS.png')
+      }
+    ]
+  }),
+  components: {
+    VueperSlides,
+    VueperSlide
   },
-  components: {},
   methods: {}
 }
 </script>
 
 <style scoped lang="scss">
-.example-slide {
-  align-items: center;
-  background-color: #666;
-  color: #999;
-  display: flex;
-  font-size: 1.5rem;
-  justify-content: center;
-  min-height: 10rem;
-}
+  .vueperslide {
+    background-size: auto;
+    background-repeat: no-repeat;
+  }
 </style>
